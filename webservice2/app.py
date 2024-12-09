c1aef72ba26b1704680981560ecd970d740855a7
Valeria Mirza <valeriamirza5@gmail.com>
2024-12-09 17:37:04 +0200

    finalfinal

--- webservice2/app.py
+++ webservice2/app.py
@@ -1,23 +1,10 @@
-from flask import Flask, jsonify, request, render_template_string
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    if request.method == 'POST':
-        username = request.form['user_input']
-        
-        # Here, you could add more sophisticated logic to check the password.
-        jsonify({"message": f"You have entered, {username}!"})
-        
-
-    return render_template_string("""
-        <form method="POST">
-            <label for="user_input">Enter your message:</label>
-            <input type="text" id="user_input" name="user_input" required>
-            
-        </form>
-    """)
+    return jsonify({"message": "Hello from webservice 2"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002)
