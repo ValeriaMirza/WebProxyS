bbdea96177946a2206bd49cd97ba45890a87d165
Valeria Mirza <valeriamirza5@gmail.com>
2024-12-09 17:03:51 +0200

    final

--- webservice2/app.py
+++ webservice2/app.py
@@ -1,10 +1,20 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template_string
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-    return jsonify({"message": "hi"})
+    if request.method == 'POST':
+        user_input = request.form['user_input']
+        return jsonify({"message": f"The message you entered is : {user_input}"})
+    
+    return render_template_string("""
+        <form method="POST">
+            <label for="user_input">Enter your message</label>
+            <input type="text" id="user_input" name="user_input">
+            <button type="submit">Submit</button>
+        </form>
+    """)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5002)
+    app.run(host="0.0.0.0", port=5001)
